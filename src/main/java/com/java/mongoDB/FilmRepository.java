c2c10efb651f58e7c0a3b21f50cc31f06f72e880
Mateusz Kućka <mateusz@MacBook-Pro-Mateusz.local>
2018-11-25 23:45:11 +0100

    repository interface updated

--- src/main/java/com/java/mongoDB/FilmRepository.java
+++ src/main/java/com/java/mongoDB/FilmRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.java.film.entity.SingleFilm;
 
 @Repository
-public class FilmRepository extends MongoRepository <SingleFilm, String> {
+public interface FilmRepository extends MongoRepository <SingleFilm, String> {
 
 }
