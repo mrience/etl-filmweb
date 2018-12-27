921ba8ddec7790e9133f0030d843eb43ca49b3b8
mateusz <kucka.mateusz14@gmail.com>
2018-12-27 23:59:10 +0100

    component scan @

--- src/main/java/com/java/app/App.java
+++ src/main/java/com/java/app/App.java
@@ -2,7 +2,10 @@ package com.java.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@ComponentScan(basePackages = "com.java")
 @SpringBootApplication
 public class App {
 	public static void main(String [] args) {
--- src/test/java/com/java/mongoDB/test/MongodbConnectionTest.java
+++ src/test/java/com/java/mongoDB/test/MongodbConnectionTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.java.configuration.MongoConfig;
 import com.java.mongoDB.FilmRepository;
 
+@ComponentScan(basePackages = "com.java")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes= {MongoConfig.class})
