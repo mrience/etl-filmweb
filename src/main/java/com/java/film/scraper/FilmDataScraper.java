811506110082c879d913041801fb88e74a0898c3
mateusz <kucka.mateusz14@gmail.com>
2019-02-07 17:04:17 +0100

    exceptions in scraper corrected

--- src/main/java/com/java/film/scraper/FilmDataScraper.java
+++ src/main/java/com/java/film/scraper/FilmDataScraper.java
@@ -47,12 +47,17 @@ public class FilmDataScraper implements Scraper{
 	}
 
 	public void scrapTitle() {
-		Element el;
-		el = doc.getElementsByClass("inline filmTitle").first().child(0);
+		Element el = null;
+		try {
+            el = doc.getElementsByClass("inline filmTitle").first().child(0);
+        } catch (NullPointerException e) {
+		    e.printStackTrace();
+		};
 		if(el != null)
-			film.setTitle(el.ownText());
-		else film.setTitle("");
-	}
+            film.setTitle(el.ownText());
+		else
+            film.setTitle("");
+    }
 	
 	public void scrapType() {
 		if(film.getUrl().contains("serial"))
@@ -67,124 +72,175 @@ public class FilmDataScraper implements Scraper{
 	}
 	
 	public void scrapPoster() {
-		Elements els;
-		els = doc.getElementsByClass("posterLightbox").select("img");
+		Elements els = null;
+		try {
+            els = doc.getElementsByClass("posterLightbox").select("img");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 		if(els != null)
 			film.setPoster(els.attr("src"));
-		else film.setPoster("");
-	}
+		else
+            film.setPoster("");
+    }
 	
 	public void scrapFilmPilot() {
-		Element el;
-		el = doc.getElementsByClass("filmPlot bottom-15").first().child(0);
-		if(el != null)
+		Element el = null;
+		try {
+            el = doc.getElementsByClass("filmPlot bottom-15").first().child(0);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (el != null)
 			film.setFilmPilot(el.ownText());
-		else 
-			film.setFilmPilot("");
-	}
+		else
+            film.setFilmPilot("");
+    }
 	
 	public void scrapDescription() {
-		Element el;
-		el = doc.getElementsByClass("pageBox filmMainDescription").first();
-		if(el != null) 
+		Element el = null;
+		try {
+            el = doc.getElementsByClass("pageBox filmMainDescription").first();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (el != null)
 			film.setDescription(el.child(0).ownText().concat(el.select("span").text()));
 		else
-			film.setDescription("");
-	}
+            film.setDescription("");
+    }
 	
 	public void scrapYear() {
 		String str;
-		Element el;
-		el = doc.getElementsByClass("inline filmTitle")
-				.first().getElementsByTag("span").first();
+		Element el = null;
+		try {
+            el = doc.getElementsByClass("inline filmTitle")
+                    .first().getElementsByTag("span").first();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 		if(el != null) {
 			str = el.ownText();
 			film.setYear(str.substring(1, str.indexOf(')')));
-		} else film.setYear("");
-	}
+		} else
+            film.setYear("");
+    }
 	
 	public void scrapFilmwebRanking() {
-		Element el = doc.getElementsByClass("worldRanking").first();
-		if(el != null) 
+	    Element el = null;
+	    try {
+            el = doc.getElementsByClass("worldRanking").first();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (el != null)
 			film.setFilmwebRanking(el.ownText());
-		 else film.setFilmwebRanking("");
-	}
+		else
+		    film.setFilmwebRanking("");
+    }
 	
 	public void scrapCommunityRate() {
-		Element el;
-		el = doc.select("[itemprop = ratingValue]").first();
-		if(el != null)
+		Element el = null;
+		try {
+            el = doc.select("[itemprop = ratingValue]").first();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (el != null)
 			film.setCommunityRate(el.text());
-			else
-				film.setCommunityRate("");
-	}
+		else
+            film.setCommunityRate("");
+    }
 	
 	public void scrapCreator() {
-		Elements els;
-		els = doc.getElementsByClass("filmInfo bottom-15")
-				.first().selectFirst("tr").select("a");
-		if(els != null)
+		Elements els = null;
+		try {
+            els = doc.getElementsByClass("filmInfo bottom-15")
+                    .first().selectFirst("tr").select("a");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (els != null)
 			film.setCreator(els.eachText());
-		else 
-			film.setCreator(List.of());
-	}
+		else
+            film.setCreator(List.of());
+    }
 	
 	public void scrapImages() {
-		Elements els;
-		els = doc.getElementsByClass("film-photos film-gallery-parent")
-				.first().getElementsByClass("zoom-1");
-		if(els != null)
+		Elements els = null;
+		try {
+            els = doc.getElementsByClass("film-photos film-gallery-parent")
+                    .first().getElementsByClass("zoom-1");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (els != null)
 			film.setImages(els.eachAttr("data-photo"));
 		else
 			film.setImages(List.of());
 	}
 	
 	public void scrapGenres() {
-		Elements els;
-		els = doc.getElementsByClass("filmInfo bottom-15")
-				.first().select("td").last().select("a");
-		if(els != null)
+		Elements els = null;
+		try {
+            els = doc.getElementsByClass("filmInfo bottom-15")
+                    .first().select("td").last().select("a");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (els != null)
 			film.setGenres(els.eachText());
 		else
-			film.setGenres(List.of());
+            film.setGenres(List.of());
 	}
 	
 	public void scrapUserReviews() {
-		Elements els;
+		Elements els = null;
 		final String PREFIX = "https://www.filmweb.pl";
 		List <String> usrReviews = new ArrayList<>();
-		els = doc.getElementsByClass("userReviews").first().select("p");
-		
-		if(els!= null) {
-			for(String str : els.select("[href^=/review]").eachAttr("href")) {
+		try {
+            els = doc.getElementsByClass("userReviews").first().select("p");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (els != null) {
+			for (String str : els.select("[href^=/review]").eachAttr("href")) {
 				usrReviews.add(PREFIX.concat(str));
 			}
 			film.setUserReviews(usrReviews);
-		}
-		else
-			film.setUserReviews(List.of());
-	}
+		} else
+            film.setUserReviews(List.of());
+    }
 	
 	public void scrapCuriosities() {
-		Elements els;
-		els = doc.getElementsByClass("sep-space text")
-				.first().getElementsByTag("span");
-		if(els != null)
-			film.setCuriosities(els.eachText());
+		Elements els = null;
+		try {
+            els = doc.getElementsByClass("sep-space text")
+                    .first().getElementsByTag("span");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+		if (els != null)
+            film.setCuriosities(els.eachText());
 		else
-			film.setCuriosities(List.of());
-	}
+            film.setCuriosities(List.of());
+    }
 	
 	public void scrapActors() {
-		Elements elements;
+		Elements elements = null;
 		Map <String, String> actors = new HashMap<>();
-		elements = doc.getElementsByClass("filmCast filmCastCast").first().getElementsByTag("tr");
-		if(elements != null) {
-			for (Element el : elements) {
-				actors.put(el.select("[itemprop = name]").text(), el.select("[itemprop = characterName]").text());
-			}
-		film.setActors(actors);
-		} else film.setActors(Map.of());
+		try {
+            elements = doc.getElementsByClass("filmCast filmCastCast").first().getElementsByTag("tr");
+		} catch (NullPointerException e) {
+		    e.printStackTrace();
+		};
+		if (elements != null) {
+            for (Element el : elements) {
+                actors.put(el.select("[itemprop = name]").text(), el.select("[itemprop = characterName]").text());
+            }
+            film.setActors(actors);
+        } else
+            film.setActors(Map.of());
 	}
 	
 	public void setFilmURL(String URL) {
